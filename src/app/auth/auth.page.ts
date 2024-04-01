d30f77260c16d6958d437f63d488302ea0082dad
MatthewComm <79467211+MatthewComm@users.noreply.github.com>
2024-04-01 13:29:59 +0200

    Added authentication form for logging in.

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -11,6 +12,7 @@ import { LoadingController } from '@ionic/angular';
 export class AuthPage implements OnInit {
 
   isLoading = false;
+  isLoginMode = true;
 
   constructor(
       private authService: AuthService,
@@ -32,6 +34,24 @@ export class AuthPage implements OnInit {
         this.router.navigateByUrl('/places/tabs/discover');
       }, 1500);
     });
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLoginMode) {
+      // Send a request to login servers
+    } else {
+      // Send a request to signup servers
+    }
+  }
+
+  onSwitchAuthMode() {
+    this.isLoginMode = !this.isLoginMode;
   }
 
 }
