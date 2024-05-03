44a51c989de0b072974b371ced7bdc4f9eabb2d5
MatthewComm <79467211+MatthewComm@users.noreply.github.com>
2024-05-03 10:17:18 +0200

    Added a form for booking a place

--- src/app/bookings/create-booking/create-booking.component.ts
+++ src/app/bookings/create-booking/create-booking.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Form, FormControl, FormGroup } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
 import { Place } from 'src/app/places/places.models';
 
@@ -10,14 +11,37 @@ import { Place } from 'src/app/places/places.models';
 export class CreateBookingComponent  implements OnInit {
 
   @Input() selectedPlace: Place;
+  @Input() selectedMode: 'select' | 'random';
+
+  startDateCtrl = new FormControl(new Date());
+
+  form: FormGroup;
+
+
 
   constructor(
     private modalController: ModalController
   ) {
     this.selectedPlace = {} as Place;
+    this.selectedMode = 'select';
+    this.form = {} as FormGroup;
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    const availableFrom = new Date(this.selectedPlace.availableFrom);
+    const availableTo = new Date(this.selectedPlace.availableTo);
+
+    console.log(availableFrom);
+    console.log(availableTo);
+
+    if (this.selectedMode === 'random') {
+      this.startDateCtrl.setValue(new Date(
+        availableFrom.getTime() +
+        Math.random() *
+        (availableTo.getTime() - 7 * 24 * 60 * 60 * 1000 - availableFrom.getTime())
+      ));
+    }
+  }
 
   onBookPlace() {
     this.modalController.dismiss({message: 'This is a dummy message!'}, 'confirm');
--- src/app/places/places-service.service.ts
+++ src/app/places/places-service.service.ts
@@ -12,21 +12,27 @@ export class PlacesService {
       'Manhattan Mansion',
       'In the heart of New York City.',
       'https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      149.99
+      149.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p2',
       'L\'Amour Toujours',
       'A romantic place in Paris!',
       'https://images.unsplash.com/photo-1486572788966-cfd3df1f5b42?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
-      189.99
+      189.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p3',
       'The Foggy Palace',
       'Not your average city trip!',
       'https://images.unsplash.com/photo-1500534314209-a25ddb2bd429?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
-      99.99
+      99.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     )
   ];
 
@@ -37,14 +43,18 @@ export class PlacesService {
       'Manhattan Mansion',
       'In the heart of New York City.',
       'https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      149.99
+      149.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p2',
       'L\'Amour Toujours',
       'A romantic place in Paris!',
       'https://images.unsplash.com/photo-1486572788966-cfd3df1f5b42?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
-      189.99
+      189.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     )
   ];
 
