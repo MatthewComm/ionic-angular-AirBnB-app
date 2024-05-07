771491804d7b5b16f3bad0e0042578d4dddc7340
MatthewComm <79467211+MatthewComm@users.noreply.github.com>
2024-05-07 07:41:58 +0200

    Added error handling to the error offer page

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Place } from '../../places.models';
 import { ActivatedRoute } from '@angular/router';
-import { LoadingController, NavController } from '@ionic/angular';
+import { LoadingController, NavController, AlertController } from '@ionic/angular';
 import { PlacesService } from '../../places-service.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
@@ -22,42 +22,56 @@ export class EditOfferPage implements OnInit, OnDestroy {
     private router: ActivatedRoute,
     private navController: NavController,
     private placesService: PlacesService,
-    private loadingController: LoadingController
+    private loadingController: LoadingController,
+    private alertCtrl: AlertController
   ) {
     this.place = {} as Place;
     this.form = new FormGroup({});
   }
 
   ngOnInit() {
-    this.router.paramMap.subscribe(paramMap => {
-      if (!paramMap.has('placeId')) {
-        this.navController.navigateBack('/places/tabs/offers');
-        return;
-      }
-      const placeId = paramMap.get('placeId')!;
-      this.isLoading = true;
-      this.placeSub = this.placesService.getPlace(placeId).subscribe((place) => {
-        this.place = place as Place;
-        this.form = new FormGroup({
-          title: new FormControl(this.place.title, {
-            updateOn: 'blur',
-            validators: [
-              Validators.required
-            ],
-          }),
-          description: new FormControl(this.place.description, {
-            updateOn: 'blur',
-            validators: [
-              Validators.required,
-              Validators.maxLength(180)
-            ]
-          }),
+    this.router.paramMap.subscribe({
+      next: (paramMap) => {
+        if (!paramMap.has('placeId')) {
+          this.navController.navigateBack('/places/tabs/offers');
+          return;
+        }
+        const placeId = paramMap.get('placeId')!;
+        this.isLoading = true;
+        this.placeSub = this.placesService.getPlace(placeId).subscribe({
+          next: (place) => {
+            this.place = place as Place;
+            this.form = new FormGroup({
+              title: new FormControl(this.place.title, {
+                updateOn: 'blur',
+                validators: [Validators.required],
+              }),
+              description: new FormControl(this.place.description, {
+                updateOn: 'blur',
+                validators: [Validators.required, Validators.maxLength(180)],
+              }),
+            });
+            this.isLoading = false;
+          },
+          error: (error) => {
+            this.alertCtrl.create({
+              header: 'An error occurred!',
+              message: 'Place could not be fetched. Please try again later.',
+              buttons: [{
+                text: 'Okay',
+                handler: () => {
+                  this.navController.navigateBack('/places/tabs/offers');
+                }
+              }]
+            }).then(alertEl => {
+              alertEl.present();
+            });
+          }
         });
-        this.isLoading = false;
-      });
-    }
-    );
+      }
+    });
   }
+
 
   onEditOffer() {
     if (!this.form.valid) {
